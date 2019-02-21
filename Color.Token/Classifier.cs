a1e706ee47c8cf8722d7e9cce1bccd97511c9d38
Benio <git@benio.me>
2019-02-21 15:13:45 +0100

    rewrite classifier filtering

--- Color.Token/Classifier.cs
+++ Color.Token/Classifier.cs
@@ -52,42 +52,33 @@ namespace Color.Token
 
 		private IList<ClassificationSpan> Classify(SnapshotSpan Span){
 			IList<ClassificationSpan> Spans = new List<ClassificationSpan>();
+
 			if (Span.IsEmpty) return Spans;
+			string Text = Span.GetText();
 
-			string SpanText = Span.GetText();
-			string[] Lines = Regex.Split(SpanText, "\n");
+			if (new Regex(@"^\t*//").IsMatch(Text)) return Spans; // C++ comment
+			if (new Regex(@"^\t*#").IsMatch(Text))  return Spans; // C directive
 
-			foreach(string Text in Lines)
-			{
-				bool IsComment = new Regex(@"^\t*//").IsMatch(Text);
-				bool IsDirective = new Regex(@"^\t*#").IsMatch(Text);
+			foreach (string Token in Meta.List){
+				foreach (Match Match in new Regex(
+						@"(?<!" + Utils.Identifier + @")"
+					+	@"(?<Token>" + Token + @")"
+					+	@"(?!"  + Utils.Identifier + @")"
+				).Matches(Text)){
+					if (Utils.IsInsideStringLiteral(
+						Text.Substring(0, Match.Groups["Token"].Index)
+					)) continue;
 
-				if(
-						!IsComment
-					&&	!IsDirective
-				){
-					foreach (string Token in Meta.List){
-						foreach (Match Match in new Regex(
-								@"(?<!" + Utils.Identifier + @")"
-							+	@"(?<Token>" + Token + @")"
-							+	@"(?!"  + Utils.Identifier + @")"
-						).Matches(Text)){
-							if(!Utils.IsInsideStringLiteral(
-								Text.Substring(0, Match.Groups["Token"].Index)
-							)){
-								var CurrentSpans = IClassifier.GetClassificationSpans(Span);
-								foreach(ClassificationSpan CurrentSpan in CurrentSpans){
-									var Name = CurrentSpan.ClassificationType.Classification;
-									if(Name == PredefinedClassificationTypeNames.Keyword){
-										Spans.Add(new ClassificationSpan(new SnapshotSpan(
-											Span.Snapshot, new Span(
-												Span.Start + Match.Groups["Token"].Index,
-												Match.Groups["Token"].Length
-											)), this.Tokens[Token]
-										));
-									}
-								}
-							}
+					var CurrentSpans = IClassifier.GetClassificationSpans(Span);
+					foreach(ClassificationSpan CurrentSpan in CurrentSpans){
+						var Name = CurrentSpan.ClassificationType.Classification;
+						if(Name == PredefinedClassificationTypeNames.Keyword){
+							Spans.Add(new ClassificationSpan(new SnapshotSpan(
+								Span.Snapshot, new Span(
+									Span.Start + Match.Groups["Token"].Index,
+									Match.Groups["Token"].Length
+								)), Tokens[Token]
+							));
 						}
 					}
 				}
