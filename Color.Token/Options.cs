842723bd3713ff10d9d10d5b82512b3634c9d176
Paweł Benetkiewicz <git@benio.me>
2019-10-16 02:27:45 +0200

    refactor: minor Signed-off-by: Paweł Benetkiewicz <git@benio.me>

--- Color.Token/Options.cs
+++ Color.Token/Options.cs
@@ -3,30 +3,30 @@ using Microsoft.VisualStudio.Shell;
 
 namespace Color.Token
 {
-	public sealed class OptionsPage
-		: DialogPage
-	{
-		[Category("Color.Token")]
-		[DisplayName("Color macros")]
-		[Description(
-				"Color macros which text is exact as tokens "
-			+	"(eg color `bool` macro defined by `stdbool.h` header in C)."
-		)]
-		public bool ColorMacro { get; } = false;
-	}
+    public sealed class OptionsPage
+        : DialogPage
+    {
+        [Category("Color.Token")]
+        [DisplayName("Color macros")]
+        [Description(
+                "Color macros which text is exact as tokens "
+            +   "(eg color `bool` macro defined by `stdbool.h` header in C)."
+        )]
+        public bool ColorMacro { get; } = false;
+    }
 
-	internal static class Options
-	{
-		// If macros should also be colored.
-		internal static bool ColorMacro
-		{
-			get
-			{
-				var Package = Project.Package;
+    internal static class Options
+    {
+        // If macros should also be colored.
+        internal static bool ColorMacro
+        {
+            get
+            {
+                var Package = Project.Package;
 
-				var Page = (OptionsPage) Package?.GetDialogPage(typeof(OptionsPage));
-				return Page != null && Page.ColorMacro;
-			}
-		}
-	}
+                var Page = (OptionsPage) Package?.GetDialogPage(typeof(OptionsPage));
+                return Page != null && Page.ColorMacro;
+            }
+        }
+    }
 }
